5ddd3112bfa130779f9b43af3d749df00a8bcecd
Pax-Newman <newmanp@wwu.edu>
2022-10-22 20:12:57 -0700

    added state stuffs

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -19,50 +19,42 @@
 import Home from './Home.svelte';
 import Scan from './Preview.svelte';
 import { onMount } from 'svelte';
-
-const STATES = {
-  home:1,
-  preview:2,
-  receipt:3,
-}
-
-let state = STATES.home
-
-// Vars
-let homeView = true;
+import { state, STATES } from './stores'
 
 // When the component is rendered
 onMount(() => {
+  // Set state based on url
   // TODO change this so it also checks if the query is valid with the API
   const querying = isParamEmpty();
-  if (querying) {
-    state = STATES.receipt
+  if (!querying) {
+    $state = STATES.receipt
   } else {
-    state = STATES.home
+    $state = STATES.home
   }
 })
 
-// Change state based on query string
+// Check if the query is empty
 function isParamEmpty() {
   const queryString = window.location.search;
   console.log(queryString);
   return queryString.length == 0
 }
-
-function toggleView (){
-  homeView = !homeView;
-}
     
 </script>
 
-<!-- Load based on  -->
+<!-- Load based on state var -->
 <body>
-  {#if state === STATES.home }
+  <button on:click={() => {$state = STATES.home}} class="button is-primary">To Home</button>
+  <button on:click={() => $state = STATES.preview} class="button is-primary">To Preview</button>
+  <button on:click={() => $state = STATES.receipt} class="button is-primary">To Receipt</button>
+  <p>{$state}</p>
+
+  {#if state === state }
     <Home/>
   {:else if state === STATES.preview}
     <Scan/>
   {:else if state === STATES.receipt}
-    <p>beeps</p>
+    <p>butts</p>
   {/if}
   <title>Home</title>
 </body>
