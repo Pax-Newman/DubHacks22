337b878f3bacf387befe911fa3d1f2de87b3de7e
nataliekeys <nataliekeys412@gmail.com>
2022-10-23 03:26:16 -0700

    stuff

--- backend/backend.py
+++ backend/backend.py
@@ -1,7 +1,9 @@
 import json
+from turtle import back
+from PIL import Image
 import backend.database as DB
 from backend.ocr_image import OCR 
-from backend.parse_receipt_text import parse_text
+from backend.parse_receipt_text import parse_receipt
 
 
 db = DB.Database()
@@ -14,6 +16,13 @@ def getReceiptData(UUID: str):
 def to_json(dic):
     return json.dumps(dic, indent = 4)  
 
+
+def createReceipt(title: str, b64_img):
+    text:str = OCR(img)
+    lines, tax = parse_receipt(text)
+    img: Image = decode_b64_img(b64_img)
+
+    db.createReceipt(title, lines, tax, img)
 
 def decode_b64_img():
     pass
