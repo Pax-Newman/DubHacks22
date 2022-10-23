bf448a3375d8e181094c10d48997dbbe05fd2877
Pax-Newman <newmanp@wwu.edu>
2022-10-22 23:07:33 -0700

    updated item card, with better style

--- client/src/ItemCard.svelte
+++ client/src/ItemCard.svelte
@@ -4,21 +4,38 @@
 
 </style>
 
-<div class="card has-background-primary " >
+<div class="box">
+  <div class="columns is-mobile is-multiline is-centered">
+    <div class="column is-three-quarter">
+      <p class="title">Oat Milk Beeswax</p>
+    </div>
+    <div class="column is-one-quarter">
+      <p class="subtitle">12.74</p>
+    </div>
+  </div>
+  <div class="columns is-multiline">
+    <div class="column is-half">
+      <button class="button is-primary">Mine!</button>
+    </div>
+    <div class="column">
+      <button class="button is-danger">Not Mine!</button>
+    </div>
+  </div>
+</div>
+
+<!-- <div class="card has-background-primary " >
   <div class="card-content">
     <div class="row">
-    <div class="columns">
+    <div class="columns is-mobiile">
  
       <div class="column is-two-thirds">
         <p class="title">Oat Milk</p>
-        <p class = "subtitle"> Dylan, Pax, Natalie</p>
-        
+        <p class="subtitle"> Dylan, Pax, Natalie</p>
       </div>
       <div class="column is-5 ">
         <p class="title" > $6.45</p>
      </div>
+    </div>
+    </div>
   </div>
-  </div>
-</div>
-
-</div>
+</div> -->
