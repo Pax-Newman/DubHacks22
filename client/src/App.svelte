eff2f925292d49e10a4d01476e584010e113981d
Pax-Newman <newmanp@wwu.edu>
2022-10-22 18:10:07 -0700

    Cleaned App and Home .svelte

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,59 +1,42 @@
-
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
-
-<title>W3.CSS Template</title>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<head>
+  <title>Home</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 
   <style>
-
-  body, html {height: 100%}
-  .bgimg {
-    min-height: 100%;
-    background-position: center;
-    background-size: cover;
-  }
-
+    body, html {height: 100%}
+    .bgimg {
+      min-height: 100%;
+      background-position: center;
+      background-size: cover;
+    }
   </style>
+</head>
 
-<head>
-  
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
-
-  <title>Home</title>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-
-  </head>
-  <body onload="isParamEmpty()">
+<body on:load={isParamEmpty}>
   {#if homeView }
-  <Home></Home>
-  {/if}
-
-  {#if !homeView }
-<Scan></Scan>
+    <Home/>
+  {:else}
+    <Scan/>
   {/if}
   <title>Home</title>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 </body>
 
 <script>
- function isParamEmpty() {
-            const queryString = window.location.search;
-            console.log(queryString);
-            if (queryString.length == 0){
-              homeView = true;
-            }else{
-              homeView = false;
-            }
-             
-            }
+function isParamEmpty() {
+  const queryString = window.location.search;
+  console.log(queryString);
+  if (queryString.length == 0) {
+    homeView = true;
+  } else {
+    homeView = false;
+  }
+}
+
 window.addEventListener('load', (event) => {
-  isParamEmpty();
+    isParamEmpty();
 });
         	import Home from './Home.svelte';
           import Scan from './Scan.svelte';
@@ -64,14 +47,6 @@ window.addEventListener('load', (event) => {
         }
     
 </script>
-
-<head>
-  <title>Home</title>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
-  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-</head>
   
 {#if homeView }
   <Home/>
--- client/src/Home.svelte
+++ client/src/Home.svelte
@@ -1,68 +1,67 @@
+<script>
 
-  
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+let homeView = true;
+let files;
 
+function isParamEmpty() {
+  const queryString = window.location.search;
+  console.log(queryString);
+  if (queryString.length == 0){
+    homeView = true;
+  } else {
+    homeView = false;
+  }               
+}
 
+window.addEventListener('load', (event) => {
+  isParamEmpty();
+});
+            
+function toggleView (){
+  homeView = !homeView;
+}
+</script>
 <head>
-  
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
-
   <title>Home</title>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 
   <style>
-
-  body, html {height: 100%}
-  .bgimg {
-    min-height: 100%;
-    background-position: center;
-    background-size: cover;
-  }
+    body, html {height: 100%}
+    .bgimg {
+      min-height: 100%;
+      background-position: center;
+      background-size: cover;
+    }
   </style>
-  </head>
+</head>
  
   
-  <div class="bgimg w3-display-container w3-animate-opacity w3-text-white">
-    <div class="w3-display-topleft w3-padding-large w3-xlarge">
-      Logo
-    </div>
-    <div class="w3-display-middle">
-      <h1 class="w3-jumbo w3-animate-top">Welcome, </h1>
-
-      <p class="w3-large w3-center"> <button class="button" onClick = "toggleView()">Button</button>
-      
-
-    </div>
-
-
-    <div class="w3-display-bottomleft w3-padding-large">
-
-    </div>
+<div class="bgimg w3-display-container w3-animate-opacity w3-text-white">
+  <div class="w3-display-topleft w3-padding-large w3-xlarge">
+    Logo
   </div>
-
-  <script>
- function isParamEmpty() {
-                const queryString = window.location.search;
-                console.log(queryString);
-                if (queryString.length == 0){
-                  homeView = true;
-                }else{
-                  homeView = false;
-                }
-                 
-                }
-    window.addEventListener('load', (event) => {
-      isParamEmpty();
-    });
-
-              import Scan from './Scan.svelte';
-              let homeView = true;
-           
-            function toggleView (){
-              homeView = !homeView;
-            }
-        
-          </script>
-
+  <div class="w3-display-middle">
+    <h1 class="w3-jumbo w3-animate-top">Welcome, </h1>
+      <label for="file-upload">
+        <span type="btn" class="button is-primary">Scan a receipt!</span>
+        <input
+              id="file-upload"
+              type="file"
+              alt="camera"
+              accept="image/*"
+              capture="environment"
+              style="display: none"
+              bind:files
+            />
+      </label>
+      {#if files && files[0]}
+        <p>
+          {files[0].name}
+        </p>
+      {/if}
+  </div>
+  <div class="w3-display-bottomleft w3-padding-large"></div>
+</div>
